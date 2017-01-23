f1c94e21cebae9cd19a684b0771c064a0748fd37
thanhnv <nguyenthanhuet@gmail.com>
2017-01-23 13:25:07 +0700

    ok

--- service.php
+++ service.php
@@ -1,7 +1,11 @@
 <?php
-
-if ($_GET['verify_token']=='thanhnv'){
-    echo $_GET['challenge'];
-}else{
-    echo 'error';
+$access_token = "129283340911236|qyHw9SrGwbV3Z2qK0GiNY3yy2YM";
+$verify_token = "thanhnv";
+$hub_verify_token = null;
+if(isset($_REQUEST['hub_challenge'])) {
+    $challenge = $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+if ($hub_verify_token === $verify_token) {
+    echo $challenge;
 }
